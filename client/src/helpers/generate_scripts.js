75ac90293eba3731afdebe6099bb0b7f7d05c205
connorwiebe <connorwiebe11@gmail.com>
2019-08-26 13:04:57 -0400

    template string syntax err fix

--- client/src/helpers/generate_scripts.js
+++ client/src/helpers/generate_scripts.js
@@ -5,19 +5,23 @@ const generateScripts = code => {
   // error script
   const errScript = document.createElement('script')
   errScript.text = `window.onerror = (msg, src, ln, col, err) => {
-    window.parent.postMessage(JSON.stringify({ console: { method: 'error', ln, msg, src, ln, col, err, args: [err.stack] } }), ${targetOrigin});
+    window.parent.postMessage(JSON.stringify({ console: { method: 'error', ln, msg, src, ln, col, err, args: [err.stack] } }), '${targetOrigin}');
   };`
+
+  console.log(errScript.text)
 
   // utilities script
   const utilsScript = document.createElement('script')
   utilsScript.text = `let log = console.log.bind(console);
   let print = console.log.bind(console);
   window.onerror = (msg, src, ln, col, err) => {
-    window.parent.postMessage(JSON.stringify({ console: { method: 'error', ln, msg, src, ln, col, err, args: [err.stack] } }), ${targetOrigin});
+    window.parent.postMessage(JSON.stringify({ console: { method: 'error', ln, msg, src, ln, col, err, args: [err.stack] } }), '${targetOrigin}');
   };
   window.onunhandledrejection = e => {
-    window.parent.postMessage(JSON.stringify({ console: { method:'error', type: 'unhandledrejection', args: [e.reason.stack]}}), ${targetOrigin});
+    window.parent.postMessage(JSON.stringify({ console: { method:'error', type: 'unhandledrejection', args: [e.reason.stack]}}), '${targetOrigin}');
   };`
+
+  console.log(utilsScript.text)
 
   // code script
   const codeScript = document.createElement('script')
